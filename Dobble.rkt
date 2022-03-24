5c7aa6af73ce156c25518e62bf279092fa1e8497
Henny35P <hans.villarroel@usach.cl>
2022-03-23 22:06:09 -0300

    Creacion archivos TDA

--- Dobble.rkt
+++ Dobble.rkt
@@ -1,2 +1,9 @@
+#lang racket
 
-"Hello world"
+;; Funcion de seleccion aleatoria, dejada de ejemplo por pauta.
+(define m 2147483647)
+(define a 1103515245)
+(define c 12345)
+
+(define randomFn (lambda (xn)
+                   (modulo (+ (* a xn) c) m)))
---
+++ TDAcardsSet.rkt
@@ -0,0 +1,3 @@
+;; Para la creacion del set de cartas se hara uso del algoritmo
+;; proporcionado por Micky Dore [https://mickydore.medium.com/the-dobble-algorithm-b9c9018afc52]
+(define (cardsSet))
