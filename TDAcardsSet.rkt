6f4bf18f7681b2171783bd5f1afc714536b8b608
Henny35P <hans.villarroel@usach.cl>
2022-03-23 23:56:23 -0300

    Como usar for/fold !

--- TDAcardsSet.rkt
+++ TDAcardsSet.rkt
@@ -5,7 +5,23 @@
 ;;
 (define elementCards 4)
 
-(define (cardsSet elementos n_elementos max_cartas randomFn)
-  (for ([i (in-range 1 elementos)])
-    (display i)))
+;; (define (cardsSet elementos n_elementos max_cartas randomFn)
+;;   (for ([i (in-range 1 elementos)])
+;;     (cons i '(1))))
 
+(define (primeraCarta elementos)
+  (for/fold ([result '()])
+            ([i (in-range 1 elementos)])
+    (cons i result)))
+
+
+
+
+
+
+
+    
+
+
+
+(primeraCarta elementCards)
