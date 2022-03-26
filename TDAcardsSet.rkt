618432e2d58b2f3cabeea37cc8c6e905c6b36c64
Henny35P <hans.villarroel@usach.cl>
2022-03-26 01:11:08 -0300

    Algoritmo escrito

--- TDAcardsSet.rkt
+++ TDAcardsSet.rkt
@@ -3,13 +3,6 @@
 ;; Para la creacion del set de cartas se hara uso del algoritmo
 ;; proporcionado por Micky Dore [https://mickydore.medium.com/the-dobble-algorithm-b9c9018afc52]
 ;;
-;;
-(define elementCards 10)
-
-;; (define (cardsSet elementos n_elementos max_cartas randomFn)
-;;   (for ([i (in-range 1 elementos)])
-;;     (cons i '(1))))
-
 
 ;; Crea la primera carta
 (define (primeraCarta elementos)
@@ -17,6 +10,8 @@
    (for/fold ([result '()])
             ([i (in-range 1 elementos)])
     (cons i result))))
+
+;; Crea un n numero de cartas
 
 (define (nCartas elementos)
   (reverse
@@ -26,15 +21,27 @@
      (cons (+ (* k elementos) (+ k 1) ) result)
      (cons j result))))
 
+; Crea un n^2 numero de cartas
 
 (define (n2Cartas elementos)
  (reverse
   (for*/fold ([result '()])
-             ([j (in-range 1 elementos)]
-              [k (in-range 1 elementos)])
-    (cons (* (- k 1) (+ elementos 2 elementos)(modulo(+ (*(- j 1) (- k 1)) (- j 1))))result)
+             ([i (in-range 1 elementos)]
+              [k (in-range 1 elementos)]
+              [j (in-range 1 elementos)])
+    (cons (modulo (+(* (* elementos (- k 1)) (+ elementos 2))(+ (* (- i 1) (- k 1))(- j 1))) elementos) result)
     (cons j result))))
 
 
+;; Funcion de ejemplo de aleatoriedad.
+(define m 2147483647)
+(define a 1103515245)
+(define c 12345)
 
-(nCartas 3)
+(define randomFn (lambda (xn)
+                   (modulo (+ (* a xn) c) m)))
+
+(randomFn 1)
+
+
+(define (cardsSet elements numE maxC (randomFn 1)))
