db3b2ef23140c595f43b1ad08d8e5668959ab9b1
Henny35P <hans.villarroel@usach.cl>
2022-03-30 22:35:34 -0300

    Empezando informe, removido for/loops

--- TDAcardsSet.rkt
+++ TDAcardsSet.rkt
@@ -4,41 +4,18 @@
 ;; proporcionado por Micky Dore [https://mickydore.medium.com/the-dobble-algorithm-b9c9018afc52]
 ;;
 
-;; Crea la primera carta
-;; (define (primeraCarta elementos)
-;;   (reverse
-;;    (for/fold ([result '()])
-;;             ([i (in-range 1 (+ elementos 2))])
-;;     (cons i result))))
-
 (define (primeraCarta elementos)
-  
   (cond
     [(= elementos 1) (list 1)]
     [(> elementos 1) (cons elementos (primeraCarta (- elementos 1)))]))
 
 
+(define (nCartas elementos)
+  (cond
+    [(= elementos 1) (list 1)]
+    [(> elementos 1) (
+                      cons elementos (primeraCarta (- elementos 1)))]))
 
-;; Crea un n numero de cartas
-
-;; (define (nCartas elementos)
-;;   (reverse
-;;    (for*/fold ([result '()])  ;; for* = nested loops
-;;        ([j (in-range 1 (+ elementos 1))]      ;
-;;         [k (in-range 1 (+ elementos 1))])
-;;      (cons (+ (* k elementos) (+ k 1) ) result)
-;;      (cons j result))))
-
-; Crea un n^2 numero de cartas
-
-;; (define (n2Cartas elementos)
-;;  (reverse
-;;   (for*/fold ([result '()])
-;;              ([i (in-range 1 (+ elementos 1))]
-;;               [j (in-range 1 (+ elementos 1))]
-;;               [k (in-range 1 (+ elementos 1))])
-;;     (cons (modulo (+(* (* elementos (- k 1)) (+ elementos 2))(+ (* (- i 1) (- k 1))(- j 1))) elementos) result)
-;;     (cons j result))))
 
 
 ;; Funcion de ejemplo de aleatoriedad.
@@ -54,7 +31,7 @@
 ;; define (cardsSet elements numE maxC randomFn))
 
 
-(define (dobble! elementos)
-  (primeraCarta elementos)
-  (nCartas elementos)
-  (n2Cartas elementos))
+;; (define (dobble! elementos)
+;;   (primeraCarta elementos)
+;;   (nCartas elementos)
+;;   (n2Cartas elementos))
--- TDAgame.rkt
+++ TDAgame.rkt
@@ -1,3 +1,11 @@
 #lang racket
 
-(define (game))
+;; (define (game))
+
+
+(define (recursion n)
+  (if  (= n 1)
+       1
+       (* n (recursion (- n 1)))))
+
+(list 1 2 3)
