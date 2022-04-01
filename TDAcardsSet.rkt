9b766f621a1edda2099011f01508a1f1503ddf11
Henny35P <hans.villarroel@usach.cl>
2022-03-31 23:27:57 -0300

    N cantidad de cartas recursiva intento

--- TDAcardsSet.rkt
+++ TDAcardsSet.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
-;; Para la creacion del set de cartas se hara uso del algoritmo
+;; Para la creacion del set de cartas se haa uso del algoritmo
 ;; proporcionado por Micky Dore [https://mickydore.medium.com/the-dobble-algorithm-b9c9018afc52]
+;;
+;; El algoritmo hace uso for loops. Para la implementacion en Racket se usara recursion
 ;;
 
 (define (primeraCarta elementos)
@@ -10,11 +12,24 @@
     [(> elementos 1) (cons elementos (primeraCarta (- elementos 1)))]))
 
 
-(define (nCartas elementos)
+(define (nCartas elementos i k)
+  (list 1)
+  (define (nCartas_k elementos)
+    (cond
+      [(= elementos 1) (list k)]
+      [(> elementos 1) (cons (+ (* elementos i ) (+ k 1)) (nCartas_k elementos i (- k 1)))]))
   (cond
-    [(= elementos 1) (list 1)]
-    [(> elementos 1) (
-                      cons elementos (primeraCarta (- elementos 1)))]))
+     [(= elementos 1) (list 1)]
+     [(> elementos 1) (cons elementos (nCartas (- elementos 1) i k))]))
+
+
+;; (cond
+;;   [(= elementos 1) (list 1)]
+;;   [(> elementos 1) (cons elementos (nCartas (- elementos 1)))]))
+;
+;
+(nCartas 30 30 30)
+
 
 
 
@@ -29,9 +44,7 @@
 
 
 ;; define (cardsSet elements numE maxC randomFn))
-
-
-;; (define (dobble! elementos)
-;;   (primeraCarta elementos)
+;;(define (dobble! elementos)
+;; (primeraCarta elementos))
 ;;   (nCartas elementos)
 ;;   (n2Cartas elementos))
